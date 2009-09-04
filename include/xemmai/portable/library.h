b75c13768b8fee73081f13bb7cc03f6900906fbb
Shin-ichi MORITA <shin1morita@gmail.com>
2009-09-04 15:23:00 +0000

    Changed to call dlopen with RTLD_GLOBAL so that cross module C++ RTTI works.
    git-svn-id: https://xemmai.googlecode.com/svn/trunk@60 21b6bbce-954d-0410-9621-597f947845d9

--- include/xemmai/portable/library.h
+++ include/xemmai/portable/library.h
@@ -28,7 +28,7 @@ public:
 	t_library() : v_handle(NULL)
 	{
 	}
-	t_library(const std::wstring& a_path) : v_handle(dlopen(f_convert(a_path + L".so").c_str(), RTLD_LAZY))
+	t_library(const std::wstring& a_path) : v_handle(dlopen(f_convert(a_path + L".so").c_str(), RTLD_LAZY | RTLD_GLOBAL))
 	{
 		if (v_handle == NULL) std::fprintf(stderr, "dlopen: %s\n", dlerror());
 	}
