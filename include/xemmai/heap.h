fd2e7b244d2abb47de861725d21846f5e65c2e5d
Shin-ichi MORITA <shin1morita@gmail.com>
2022-11-29 07:26:10 +0900

    Fixed a race condition on accumulating v_returned.
    f_return() is called on mutators.
    f_flush() is called on the collector.

--- include/xemmai/heap.h
+++ include/xemmai/heap.h
@@ -45,7 +45,7 @@ class t_heap
 		std::atomic<T*> v_chunks = nullptr;
 		std::atomic_size_t v_grown = 0;
 		std::atomic_size_t v_allocated = 0;
-		size_t v_returned = 0;
+		std::atomic_size_t v_returned = 0;
 		size_t v_freed = 0;
 
 		T* f_grow(t_heap& a_heap)
@@ -86,7 +86,7 @@ class t_heap
 			p->v_previous = v_chunks.load(std::memory_order_relaxed);
 			while (!v_chunks.compare_exchange_weak(p->v_previous, p, std::memory_order_release));
 			v_head<A_rank> = nullptr;
-			v_returned += a_n;
+			v_returned.fetch_add(a_n, std::memory_order_relaxed);
 		}
 		void f_return()
 		{
@@ -107,7 +107,7 @@ class t_heap
 		}
 		size_t f_live() const
 		{
-			return v_allocated.load(std::memory_order_relaxed) - v_returned - v_freed;
+			return v_allocated.load(std::memory_order_relaxed) - v_returned.load(std::memory_order_relaxed) - v_freed;
 		}
 	};
 
@@ -160,11 +160,11 @@ public:
 	template<typename T_each>
 	void f_statistics(T_each a_each) const
 	{
-		a_each(size_t(0), v_of0.v_grown.load(std::memory_order_relaxed), v_of0.v_allocated.load(std::memory_order_relaxed), v_of0.v_returned);
-		a_each(size_t(1), v_of1.v_grown.load(std::memory_order_relaxed), v_of1.v_allocated.load(std::memory_order_relaxed), v_of1.v_returned);
-		a_each(size_t(2), v_of2.v_grown.load(std::memory_order_relaxed), v_of2.v_allocated.load(std::memory_order_relaxed), v_of2.v_returned);
-		a_each(size_t(3), v_of3.v_grown.load(std::memory_order_relaxed), v_of3.v_allocated.load(std::memory_order_relaxed), v_of3.v_returned);
-		a_each(size_t(4), v_of4.v_grown.load(std::memory_order_relaxed), v_of4.v_allocated.load(std::memory_order_relaxed), v_of4.v_returned);
+		a_each(size_t(0), v_of0.v_grown.load(std::memory_order_relaxed), v_of0.v_allocated.load(std::memory_order_relaxed), v_of0.v_returned.load(std::memory_order_relaxed));
+		a_each(size_t(1), v_of1.v_grown.load(std::memory_order_relaxed), v_of1.v_allocated.load(std::memory_order_relaxed), v_of1.v_returned.load(std::memory_order_relaxed));
+		a_each(size_t(2), v_of2.v_grown.load(std::memory_order_relaxed), v_of2.v_allocated.load(std::memory_order_relaxed), v_of2.v_returned.load(std::memory_order_relaxed));
+		a_each(size_t(3), v_of3.v_grown.load(std::memory_order_relaxed), v_of3.v_allocated.load(std::memory_order_relaxed), v_of3.v_returned.load(std::memory_order_relaxed));
+		a_each(size_t(4), v_of4.v_grown.load(std::memory_order_relaxed), v_of4.v_allocated.load(std::memory_order_relaxed), v_of4.v_returned.load(std::memory_order_relaxed));
 		a_each(size_t(V_RANKX), size_t(0), v_allocated, v_freed);
 	}
 	XEMMAI__PORTABLE__ALWAYS_INLINE constexpr T* f_allocate(size_t a_size)
--- test/race.xm
+++ test/race.xm
@@ -15,30 +15,9 @@ good_bye = @(name, n)
 	return @
 		hello(name, n
 
-foo = Thread(good_bye("foo", 30
-bar = Thread(good_bye("bar", 20
-zot = Thread(good_bye("zot", 10
-fooo = Thread(good_bye("fooo", 30
-baar = Thread(good_bye("baar", 20
-zoot = Thread(good_bye("zoot", 10
-foooo = Thread(good_bye("foooo", 30
-baaar = Thread(good_bye("baaar", 20
-zooot = Thread(good_bye("zooot", 10
-fooooo = Thread(good_bye("fooooo", 30
-baaaar = Thread(good_bye("baaaar", 20
-zoooot = Thread(good_bye("zoooot", 10
-
-foo.join(
-bar.join(
-zot.join(
-fooo.join(
-baar.join(
-zoot.join(
-foooo.join(
-baaar.join(
-zooot.join(
-fooooo.join(
-baaaar.join(
-zoooot.join(
+threads = [
+for i = 0; i < 20; i = i + 1
+	threads.push(Thread(good_bye("thread " + i, (i % 3 + 1) * 10
+threads.each(@(x) x.join(
 
 print("counter: " + counter
